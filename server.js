040ceb0f9a6a9a44826cf60e68c33c66a32c9f01
morag4556 <morag4556@gmail.com>
2025-04-05 16:50:41 -0700

    html.login.server

---
+++ server.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public')); // Serve static files from 'public' folder
+
+// Routes
+app.post('/submit-contact', (req, res) => {
+  console.log('Contact form data:', req.body);
+  res.send('Thanks for contacting us!');
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  console.log('Login attempt:', email, password);
+  // Add authentication logic here
+  res.send(`Welcome, ${email}!`);
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
